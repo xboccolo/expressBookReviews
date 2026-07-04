e562d277f78a902301c353dc150a8f099afb5ed3
xboccolo <you@example.com>
2026-07-04 13:57:24 +0200

    books by author method

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,37 @@ public_users.post("/register", (req,res) => {
 
 // Get method for showing the book list available in the shop
 public_users.get('/', (req, res) => {
-  res.send(JSON.stringify(friends, null, 4));
+  res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  } else {
+    return res.status(404).json({message: "Book not found"});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  let author = req.params.author;
+
+  let keys = Object.keys(books);
+  let booksByAuthor = [];
+  for (let i=0; i<keys.length; i++) {
+    if (books[isbn].author.toLowerCase() === author.toLowerCase()) {
+      booksByAuthor.push(books[isbn]);
+    }
+  }
+
+  if (booksByAuthor.length > 0) {
+    return res.status(200).json(booksByAuthor);
+  } else {
+    return res.status(404).json({message: "No books found"});
+  }
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
