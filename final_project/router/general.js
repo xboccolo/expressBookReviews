da42dfb2125f26177fd22ab057caa38fdd0873ba
xboccolo <you@example.com>
2026-07-04 13:26:13 +0200

    dependencies installed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,9 +11,8 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', (req, res) => {
+  res.send(JSON.stringify(friends, null, 4));
 });
 
 // Get book details based on ISBN
