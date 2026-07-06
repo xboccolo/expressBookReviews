fde116356f517ad4dcf24e2df677e9ca30aee5ef
xboccolo <you@example.com>
2026-07-06 14:20:44 +0200

    async/await for isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -55,6 +55,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
     return res.status(404).json({message: "Book not found"});
   }
  });
+
+ // async prima della funzione (req, res) => {...}
+ //
+
+ public_users.get('/isbn/:isbn', async (req, res) => {
+  try {
+    // Aspetto che la promessa si risolva e salvo il valore
+    const book = await fetchBookByIsbn; 
+    return res.status(200).json(risultato);
+  } catch (errore) {
+    // Gestisco il fallimento
+    return res.status(500).json({ message: errore });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
