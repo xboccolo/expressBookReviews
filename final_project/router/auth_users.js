a6440dd9c8ca682101c26ffc564fec57602576fc
xboccolo <you@example.com>
2026-07-06 12:57:10 +0200

    updated to task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -36,14 +36,60 @@ const authenticatedUser = (username, password) => {
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (authenticatedUser(username, password)) {
+      let accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60});
+      req.session.authorization = {accessToken, username}
+      return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+  }
+  return res.status(404).json({ message: "Error logging in" });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const reviewText = req.query.review;
+  // Se l'utente non è loggato.
+  if (!req.session.authorization) {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+  const { username } = req.session.authorization;
+  // se manca il parametro recensione.
+  if (!reviewText) {
+    return res.status(400).json({message: "Review must be provided"});
+  }
+  // se il libro non esiste.
+  if (!books[isbn]) {
+    return res.status(404).json({message: "Book not found"});
+  }
+  // ora aggiunge o sovrascrive la recensione de determinato utente.
+  books[isbn].reviews[username] = reviewText;
+  return res.status(200).json({ 
+    message: `Review for '${books[isbn].title}' successfully added/updated by user ${username}` 
+  });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const { username } = req.session.authorization;
+  // se il libro non esiste.
+  if (!books[isbn]) {
+    return res.status(404).json({message: "Book not found"});
+  }
+  // se non esiste la recensione.
+  if (!books[isbn].reviews[username]) {
+    return res.status(404).json({message: "Review not found"});
+  }
+  // ora cancella la recensione.
+  delete books[isbn].reviews[username];
+  return res.status(200).json({
+      message: `Review for '${books[isbn].title}' successfully deleted by user ${username}`
+  });
 });
 
 module.exports.authenticated = regd_users;
